b21ce7a1116aff63f4a2382058377e57cfa6b6dd
yuliazhydchenko <juliazhydchenko@gmail.com>
2017-04-26 23:31:44 +0300

    add MyClass

---
+++ Lab1_PT/SportEvents/Laboratorna1/MyClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorna1
+{
+    class MyClass
+    {
+
+        public static void main(String[] args) {
+          
+            Console.WriteLine("Hello from master branch after merge");
+        }
+    }
+}
